98af98c767d3ace701180851bcce50360cde2d58
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-20 13:03:42 +0200

    FibonacciArr created, fizzbuzz map created

--- src/fibsFizzBuzz.js
+++ src/fibsFizzBuzz.js
@@ -1,5 +1,43 @@
-function fibsFizzBuzz() {
-  return 'fibsFizzBuzz';
+function fibsFizzBuzz(num) {
+
+// Create fibonacci sequence array
+
+let fibonacciArr = [];
+
+console.log('fibonacciArr length: ', fibonacciArr.length)
+
+// 1, 1, 2, 3, 5, 8, 13
+
+if ( num === 0 ) fibonacciArr = [];
+if ( num === 1) fibonacciArr = [1]
+if (num === 2) fibonacciArr = [1, 1]
+
+if (num > 2){
+  fibonacciArr = [1, 2]
+  console.log('fibonacciArr length: ', fibonacciArr.length)
+
+  for (let i = fibonacciArr.length; i < num; i++){
+    fibonacciArr.push(fibonacciArr[i - 2] + fibonacciArr[i-1])
+  }
+}
+
+
+console.log('fibonacciArr: ', fibonacciArr)
+
+
+// Replace numbers in the sequence
+
+
+let result = fibonacciArr.map((number) => {
+  if (number % 5 === 0 && number % 3 === 0 ) return 'FizzBuzz';
+  if (number % 5 === 0 ) return 'Buzz';
+  if (number % 3 === 0 ) return 'Fizz';
+  return number;
+})
+
+console.log('Result: ', result)
+
+  return result;
 }
 
 module.exports = fibsFizzBuzz;
--- src/fibsFizzBuzz.test.js
+++ src/fibsFizzBuzz.test.js
@@ -2,6 +2,23 @@ const fibsFizzBuzz = require('./fibsFizzBuzz');
 
 describe('fibsFizzBuzz', () => {
   it('should return fibsFizzBuzz', () => {
-    expect(fibsFizzBuzz()).toBe('fibsFizzBuzz');
+    expect(fibsFizzBuzz(5)).toStrictEqual('fibsFizzBuzz');
+  });
+
+  it('should return fibsFizzBuzz', () => {
+    expect(fibsFizzBuzz(0)).toStrictEqual('fibsFizzBuzz');
+  });
+
+  it('should return fibsFizzBuzz', () => {
+    expect(fibsFizzBuzz(1)).toStrictEqual('fibsFizzBuzz');
+  });
+
+  it.only('2 should return fibs[ 1, 1 ]', () => {
+    expect(fibsFizzBuzz(2)).toStrictEqual([ 1, 1 ]);
+  });
+
+
+  it('should return fibsFizzBuzz', () => {
+    expect(fibsFizzBuzz(10)).toStrictEqual('fibsFizzBuzz');
   });
 });
