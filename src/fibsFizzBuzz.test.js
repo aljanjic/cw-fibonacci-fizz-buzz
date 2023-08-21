1218ce5abc9a693f7af28860eac24c9ce3075e19
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-21 09:14:59 +0200

    Setting up the tests

--- src/fibsFizzBuzz.test.js
+++ src/fibsFizzBuzz.test.js
@@ -1,24 +1,24 @@
 const fibsFizzBuzz = require('./fibsFizzBuzz');
 
 describe('fibsFizzBuzz', () => {
-  it('should return fibsFizzBuzz', () => {
-    expect(fibsFizzBuzz(5)).toStrictEqual('fibsFizzBuzz');
+  it('5 should return [1,1,2,"Fizz","Buzz"]', () => {
+    expect(fibsFizzBuzz(5)).toStrictEqual([1,1,2,"Fizz","Buzz"]);
   });
 
-  it('should return fibsFizzBuzz', () => {
-    expect(fibsFizzBuzz(0)).toStrictEqual('fibsFizzBuzz');
+  it('0 should return  []', () => {
+    expect(fibsFizzBuzz(0)).toStrictEqual([]);
   });
 
-  it('should return fibsFizzBuzz', () => {
-    expect(fibsFizzBuzz(1)).toStrictEqual('fibsFizzBuzz');
+  it('1 should return  [1]', () => {
+    expect(fibsFizzBuzz(1)).toStrictEqual([1]);
   });
 
-  it.only('2 should return fibs[ 1, 1 ]', () => {
+  it('2 should return [ 1, 1 ]', () => {
     expect(fibsFizzBuzz(2)).toStrictEqual([ 1, 1 ]);
   });
 
 
-  it('should return fibsFizzBuzz', () => {
-    expect(fibsFizzBuzz(10)).toStrictEqual('fibsFizzBuzz');
+  it('10 should return [1,1,2,"Fizz","Buzz",8,13,"Fizz",34,"Buzz"]', () => {
+    expect(fibsFizzBuzz(10)).toStrictEqual([1,1,2,"Fizz","Buzz",8,13,"Fizz",34,"Buzz"]);
   });
 });
